ac10a8dcdfb7a243887d8471c4df68e04f7790bb
Jeyvison Nascimento <jeynoronha@gmail.com>
2021-03-30 10:43:22 -0300

    Commit inicial

---
+++ modules/apps/liferay-learn/liferay-learn-taglib/src/main/java/com/liferay/lifraylearn/taglib/servlet/taglib/base/BaseURLTag.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.lifraylearn.taglib.servlet.taglib.base;
+
+import com.liferay.lifraylearn.taglib.internal.servlet.ServletContextUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * @author Jeyvison Nascimento
+ * @author Leonardo Barros
+ * @generated
+ */
+public abstract class BaseURLTag extends com.liferay.taglib.util.IncludeTag {
+
+	@Override
+	public int doStartTag() throws JspException {
+		setAttributeNamespace(_ATTRIBUTE_NAMESPACE);
+
+		return super.doStartTag();
+	}
+
+	public java.lang.String getComponentId() {
+		return _componentId;
+	}
+
+	public void setComponentId(java.lang.String componentId) {
+		_componentId = componentId;
+	}
+
+	@Override
+	public void setPageContext(PageContext pageContext) {
+		super.setPageContext(pageContext);
+
+		setServletContext(ServletContextUtil.getServletContext());
+	}
+
+	@Override
+	protected void cleanUp() {
+		super.cleanUp();
+
+		_componentId = null;
+	}
+
+	@Override
+	protected String getEndPage() {
+		return _END_PAGE;
+	}
+
+	@Override
+	protected String getStartPage() {
+		return _START_PAGE;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest request) {
+		setNamespacedAttribute(request, "componentId", _componentId);
+	}
+
+	protected static final String _ATTRIBUTE_NAMESPACE = "liferay-learn:url:";
+
+	private static final String _END_PAGE =
+		"/url/end.jsp";
+
+	private static final String _START_PAGE =
+		"/url/start.jsp";
+
+	private java.lang.String _componentId = null;
+
+}
