00a8c9bf5b07191a11e947867fdb3d7eebbcdca6
Alejo Ceballos <alejo.ceballos@liferay.com>
2021-03-30 18:48:37 -0300

    Taglib Code

---
+++ modules/apps/liferay-learn/liferay-learn-taglib/src/main/java/com/liferay/liferay/learn/taglib/servlet/taglib/base/BaseUrlTag.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.liferay.learn.taglib.servlet.taglib.base;
+
+import com.liferay.liferay.learn.taglib.internal.servlet.ServletContextUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * @author Jeyvison Nascimento
+ * @author Alejo Ceballos
+ * @generated
+ */
+public abstract class BaseUrlTag extends com.liferay.taglib.util.IncludeTag {
+
+	@Override
+	public int doStartTag() throws JspException {
+		setAttributeNamespace(_ATTRIBUTE_NAMESPACE);
+
+		return super.doStartTag();
+	}
+
+	public java.lang.String getKey() {
+		return _key;
+	}
+
+	public void setKey(java.lang.String key) {
+		_key = key;
+	}
+
+	@Override
+	public void setPageContext(PageContext pageContext) {
+		super.setPageContext(pageContext);
+
+		setServletContext(ServletContextUtil.getServletContext());
+	}
+
+	@Override
+	protected void cleanUp() {
+		super.cleanUp();
+
+		_key = null;
+	}
+
+	@Override
+	protected String getEndPage() {
+		return _END_PAGE;
+	}
+
+	@Override
+	protected String getStartPage() {
+		return _START_PAGE;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest request) {
+		setNamespacedAttribute(request, "key", _key);
+	}
+
+	protected static final String _ATTRIBUTE_NAMESPACE = "liferay-liferay-learn:url:";
+
+	private static final String _END_PAGE =
+		"/url/end.jsp";
+
+	private static final String _START_PAGE =
+		"/url/start.jsp";
+
+	private java.lang.String _key = null;
+
+}
---
+++ modules/apps/liferay-learn/liferay-learn-taglib/src/main/resources/META-INF/resources/init.jsp
@@ -0,0 +1,29 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+@generated
+--%>
+<%@ page import="com.liferay.portal.kernel.servlet.taglib.aui.ScriptData"%>
+<%@ page import="com.liferay.portal.kernel.util.GetterUtil" %>
+
+<%@ page import="java.io.Serializable" %>
+
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="java.util.Calendar" %>
+<%@ page import="java.util.Date" %>
+<%@ page import="java.util.HashMap" %>
+<%@ page import="java.util.Locale" %>
+<%@ page import="java.util.Map" %>
+<%@ page import="java.util.Set" %>
